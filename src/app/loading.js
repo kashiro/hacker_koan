a44eec7cebe31d20064bf7781b9fefe4d817dcd6
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2017-11-03 17:44:56 +0900

    feat(view): added loading indicator (#13)

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import {h, Component} from 'preact';
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div class="loading flex flex-center"><p>Loading...</p></div>
+    );
+  }
+}
