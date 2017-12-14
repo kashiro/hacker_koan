c1e4ced06664aa0314b59bc013f6dd38428b3489
tomoyukikashiro <ksrtmyk@gmail.com>
2017-12-14 11:42:08 +0900

    feat(sw): Exclude index.html not to support offline
    GH-22

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
-const cacheName = 'hacker_koans-v-4';
+const cacheName = 'hacker_koans-v-1.0.5';
 const filesToCache = [
   '/',
-  '/index.html',
   '/app.bundle.js',
   '/font.bundle.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
+  'https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/fonts/devicon.woff?-hdf3wh',
+  'https://fonts.gstatic.com/s/cutivemono/v6/N5odNRruTwjvCM8y77PhQaE8kM4xWR1_1bYURRojRGc.woff2',
+  'https://fonts.gstatic.com/s/cutivemono/v6/N5odNRruTwjvCM8y77PhQYgp9Q8gbYrhqGlRav_IXfk.woff2',
 ];
 
 self.addEventListener('install', e => {
